72b5e87b4a8b4f7f7f66a58b5516cae9944db41a
Sune Jakobsson <sune.jakobsson@gmail.com>
2018-01-26 15:54:05 +0100

    Updates

--- src/R/try_forecast.R
+++ src/R/try_forecast.R
@@ -1,9 +1,12 @@
 #
 # with inspiration from: http://a-little-book-of-r-for-time-series.readthedocs.io/en/latest/src/timeseries.html
 #
-indat <- scan('week_42_2017.log', what=list("","","","","","","","","","","","","","","","",""), flush=TRUE)
+indat <- scan('January_2018.log', what=list("","","","","","","","","","","","","","","","",""), flush=TRUE)
+str(indat)
 
 totalTime <- as.numeric(indat[[2]]);
+freeMem <- as.double(indat[[11]]);
+findfrequency(freeMem);
 
 minuteInput <- unlist(strsplit(indat[[9]], "Check=", fixed = TRUE))[c(FALSE,TRUE)];
 # Get the day of the invocation
@@ -12,8 +15,16 @@ dayInput <- substr(indat[[12]], 7, 16);
 sampleTime <- paste(dayInput, minuteInput);
 # Convert to time input 
 timeForSample <- strptime( sampleTime, "%Y_%m_%d %H_%M_%S");
+summary(timeForSample)
 
-df <- data.frame(timeForSample, freeMem)
+df <- data.frame(timeForSample, freeMem);
+findfrequency(df);
+findfrequency(freeMem);
+
+# when did we run on instance 2
+timeForSample[indat[[17]] == '2']
+
+
 
 iii <- ts(freeMem, frequency=1440, start=c(0,0))
 plot(decompose(iii, type="multiplicative"))
